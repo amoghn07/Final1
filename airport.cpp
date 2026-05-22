967ae369bc091d4d43594fec63d06e56fee3c3a0
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-05-21 19:00:43 -0700

    update

---
+++ airport.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main(){
+    ifstream fin;
+    map <string, int> air;
+
+    //opening file
+    fin.open("210-final-1-SP26.txt");
+    //error handling
+    if (!fin) {
+        cerr << "Error opening file." << endl;
+        return 1;
+    }
+
+    string start;
+    string dest;
+
+    //while loop to read in air ports and count up number of times
+    //each airport visited.
+    while (fin >> start) {
+        fin >> dest;
+        air[start]++;
+        air[dest]++;
+    }
+    //range based for loop to print out map
+    for (a : air) {
+        
+    }
+
+
+
+    //closing file
+    fin.close();
+    return 0;
+}
